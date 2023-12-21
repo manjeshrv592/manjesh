fb5540acea010d0de0c653f563d7479036ff4e17
Manjesh R V <manjeshrv592@gmail.com>
2023-12-21 15:27:13 +0530

    Added logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,13 +1,23 @@
 import Link from 'next/link';
 import DesktopNav from './DesktopNav';
 import MobileNav from './MobileNav';
+import Image from 'next/image';
 
 const Header = () => {
   return (
     <header className='absolute left-0 top-0 w-full py-5'>
       <div className='container'>
         <div className='flex items-center justify-between'>
-          <Link href='/'>Logo</Link>
+          <Link href='/'>
+            <Image
+              className='md:w-14'
+              src='/assets/logos/my-logo.svg'
+              alt='Manjesh logo'
+              width={40}
+              height={40}
+              sizes='32px, (min-width: 768px) 64px'
+            />
+          </Link>
           <DesktopNav />
           <MobileNav />
         </div>
